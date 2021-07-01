4e03c214ed78a836bd9ad181e024e6022580b631
Neoniq <demon9836@gmail.com>
2021-07-01 16:02:26 +0300

    final

---
+++ 04-board-sources/app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#00EAD3', '#FFF5B7', '#FF449F', '#005F99']
+const SQUARES_NUMBER = 1566
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => setColor(square))
+
+  square.addEventListener('mouseleave', () => removeColor(square))
+
+  board.append(square)
+}
+function setColor(element) {
+  const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d'
+  element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
+}
